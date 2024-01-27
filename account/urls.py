b33aba8c32ab03efff8864e98cb7423d8c245abd
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-27 17:03:38 +0100

    Update dependencies and URLs configuration

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import (
     register,
     custom_login,
@@ -14,7 +17,10 @@ urlpatterns = [
     path('logout/', custom_logout, name='logout'),
     path('login/', custom_login, name='login'),
     path('checkout/', checkout, name='checkout'),
-    path('create-sub/', create_sub, name='create_sub'),  # Updated this line
+    path('create-sub/', create_sub, name='create_sub'),
     path('complete/', complete, name='complete'),
     path('cancel/', cancel, name='cancel'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
